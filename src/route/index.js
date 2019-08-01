d8ee3f1b22bba0cf2ebdffe5f9c3e11c86fe693e
rschmidt <ramonroc@gmail.com>
2019-08-01 18:50:50 -0300

    Add middleware to redirect to events list when user authenticated

--- src/route/index.js
+++ src/route/index.js
@@ -1,14 +1,26 @@
 import React, { lazy } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import WaitComponent from 'components/Wrappers/WaitComponent';
 import PrivateRoute from 'route/PrivateRoute';
 import URL from 'route/URL';
+import AuthService from 'services/Auth';
 
 const EventListPage = lazy(() => import('pages/EventListPage/EventListPage'));
 const LoginPage = lazy(() => import('pages/Login/LoginPage'));
 
 const Router = () => (
   <BrowserRouter>
+    <Route
+      exact
+      path={URL.AUTH.LOGIN}
+      render={() =>
+        AuthService.isAuthenticated() ? (
+          <Redirect to={URL.EVENTS.LIST} />
+        ) : (
+          <Redirect to={URL.AUTH.LOGIN} />
+        )
+      }
+    />
     <Route path={URL.AUTH.LOGIN} exact component={WaitComponent(LoginPage)} />
     <PrivateRoute
       path={URL.EVENTS.LIST}
