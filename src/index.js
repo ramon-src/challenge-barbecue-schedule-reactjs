ac327cb27f17b5df2e357bb670ee3661d0e6f165
Ramon Schmidt <rschmidt@umov.me>
2019-08-02 18:35:55 -0300

    Add Material theme provider to App

--- src/index.js
+++ src/index.js
@@ -7,12 +7,25 @@ import { createStore } from 'redux';
 import { StoreContext } from 'redux-react-hook';
 import reducers from 'store/reducers';
 
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+let theme = createMuiTheme({
+  palette: {
+    primary: { main: '#ffca28' },
+    secondary: { main: '#ffc400' }
+  }
+});
+theme = responsiveFontSizes(theme);
+
 const store = createStore(reducers);
 store.subscribe(() => console.log(store.getState()));
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
@@ -20,4 +33,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
